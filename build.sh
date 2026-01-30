d1c4d817a524c91e129d07c22f9092a892088bc1
Brent-Mendoza <mjustinebrent@gmail.com>
2026-01-30 18:17:50 +0800

    add: build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Clone Flutter if not exists
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+# Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Enable web
+flutter config --enable-web
+
+# Get dependencies
+flutter pub get
+
+# Build for web
+flutter build web --release --verbose
